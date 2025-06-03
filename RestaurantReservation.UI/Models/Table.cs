8c34993da1e0c80d591ebc64391232c532c33796
Artem Shtopo <shtopo1989@gmail.com>
2025-06-03 19:39:54 +0300

    Видалив Blazor UI, додав власний UI

--- RestaurantReservation.UI/Models/Table.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantReservation.UI.Models
-{
-    public class Table
-    {
-        public int Number { get; set; }
-        public int Seats { get; set; }
-        public bool IsAvailable { get; set; }
-    }
-}
